ef25e23e1e5e65b66874669ec5908cdb69ab1efd
Thái <nnt000fit@gmail.com>
2026-02-28 15:11:33 +0700

    fix header mobile

--- js/header.js
+++ js/header.js
@@ -17,9 +17,35 @@ let head = `        <div class="header-container">
                     <input type="text" placeholder="Quick search">
                     <div class="nav-mobile-search-img" id="btn-search-mobile"><img src="img/home/nav-ico-search.svg" alt=""></div>
                 </div>
-                <div class="nav-mobile-list">
+                <div class="nav-mobile-list" id="btn-list-mobile">
                     <img src="img/home/nav-ico-list.svg" alt="">
                 </div>
             </div>
+        </div>
+        <div class="list-mobile" id="list-mobile">
+            <div class="list-mobile-container" id="list-mobile-nav">
+                <div class="list-mobile-container-close" id="list-mobile-close"></div>
+                <a href="" class="list-mobile-text">Trang chủ</a>
+                <a href="" class="list-mobile-text">Khám phá</a>
+                <a href="" class="list-mobile-text">Trò chơi</a>
+                <a href="" class="list-mobile-text">Đăng nhập</a>
+            </div>
         </div>`
 document.getElementsByTagName("header")[0].innerHTML = head;
+
+
+
+const btnList = document.getElementById("btn-list-mobile");
+const btnListClose = document.getElementById("list-mobile-close");
+const displayListNav = document.getElementById("list-mobile-nav");
+const displayList = document.getElementById("list-mobile");
+btnList.addEventListener("click", () =>{
+    displayList.classList.add("active");
+    displayListNav.classList.add("active");
+});
+displayList.addEventListener("click", (e) => {
+    if (e.target === displayList || e.target == btnListClose) {
+        displayList.classList.remove("active");
+        displayListNav.classList.remove("active");
+    }
+});
